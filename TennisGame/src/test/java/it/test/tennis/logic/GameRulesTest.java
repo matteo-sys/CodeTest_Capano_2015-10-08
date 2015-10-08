333cdc83641caec926f4d5cee067dd062af3dd38
Matteo Capano <matteo.capano@sysdata.it>
2015-10-08 18:50:42 +0200

    test

--- TennisGame/src/test/java/it/test/tennis/logic/GameRulesTest.java
+++ TennisGame/src/test/java/it/test/tennis/logic/GameRulesTest.java
@@ -1,19 +1,36 @@
 package it.test.tennis.logic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import it.test.tennis.model.Player;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameRulesTest {
-
-	@Test
-	public void testGetScoringPlayer() {
-		fail("Not yet implemented");
+	// local variable
+	GameRules rules;
+	// static
+	static int id;
+	
+	@Before
+	public void initGame() {
+		rules = new GameRules();
+		id = 0;
+	}
+	
+	// utility method
+	private static Player buildPlayerWithScore(int score) {
+		return new Player("Player"+id, score);
 	}
 
 	@Test
 	public void testCheckAdvantage() {
-		fail("Not yet implemented");
+		assertNull(rules.checkAdvantage(buildPlayerWithScore(0), buildPlayerWithScore(0)));
+		assertNull(rules.checkAdvantage(buildPlayerWithScore(0), buildPlayerWithScore(1)));
+		assertNull(rules.checkAdvantage(buildPlayerWithScore(1), buildPlayerWithScore(0)));
+		assertNull(rules.checkAdvantage(buildPlayerWithScore(1), buildPlayerWithScore(1)));
+		// TODO: etc.
 	}
 
 	@Test
